faba3a5ae940d17a0d34d0f492ed6cdca1b7691c
Rohith <98480349+Rozrc25@users.noreply.github.com>
2024-05-31 19:42:50 +0530

    Update content_option.js

--- content_option.js
+++ content_option.js
@@ -1,12 +1,12 @@
 import rohith from "./assets/images/avarohith.jpg"
-import thumbnail from './assets/images/RF-home.png'
+import thumbnail from "./assets/images/thumbnail.png"
 import walmart from "./assets/images/walmart ASEVEP.jpg"
 import accent from "./assets/images/accenture DVEP.jpg"
 import cloud from "./assets/images/europ cloud.jpg"
 import camb from "./assets/images/BEC interm.jpg"
-import spktl from './assets/images/spok java.jpg'
-import tothumb from "./assets/images/todo.jpg"
-var ceticon ="https://img.favpng.com/6/20/9/diploma-higher-education-logo-academic-certificate-png-favpng-t9LrfKwr2GdHpV088Ph3dQ5NF.jpg"
+import nptel from "./assets/images/nptel.jpg"
+import deloitte from "./assets/images/Deloitte=Forage.png"
+var ceticon = "https://img.favpng.com/6/20/9/diploma-higher-education-logo-academic-certificate-png-favpng-t9LrfKwr2GdHpV088Ph3dQ5NF.jpg"
 
 const logotext = "ROZRC25";
 const meta = {
@@ -28,86 +28,89 @@ const introdata = {
 
 const dataabout = {
     title: "about my self",
-    aboutme:"I am Rohith, a final-year B.Tech CSE student. Driven by a passion for technology, I possess a strong foundation in programming, software development, and data structures. With excellent problem-solving abilities and effective communication skills, I thrive in dynamic environments. Eager to contribute to innovative projects, I am dedicated to continuous learning and staying ahead in the rapidly evolving tech industry.",
+    aboutme: "I am Rohith, a final-year B.Tech CSE student. Driven by a passion for technology, I possess a strong foundation in programming, software development, and data structures. With excellent problem-solving abilities and effective communication skills, I thrive in dynamic environments. Eager to contribute to innovative projects, I am dedicated to continuous learning and staying ahead in the rapidly evolving tech industry.",
 };
 const certificates = [{
-        jobtitle: "Advanced Software Engineering Virtual Experience Program",
-        where: "(Walmart Global Tech)",
-        date: "2023",
-        link:"https://drive.google.com/file/d/1ULl-qAOyFpatIcZXsWdRNt_u-clJGxui/view?usp=sharing",
-       src:walmart
-    },
-    {
-        jobtitle: "Accenture Nordics Virtual Experience Program",
-        where: "Forage(Developer)",
-        date: "2023",
-        link:"https://drive.google.com/file/d/1dYLcCCmC6LnG1YlCU7qCJaI24zPbLaf3/view?usp=sharing",
-        src:accent
-    },
-    {
-        jobtitle: "Certified Cloud Computing ",
-        where: "(Europe Academy)",
-        date: "2021",
-        link:"https://drive.google.com/file/d/1zs10WXvnmVA8_awXAvDXq43tYaCNZfqX/view?usp=sharing",
-        src:cloud
-    },
-    {
-        jobtitle: "Business English",
-        where: "Cambridge English",
-        date: "2023",
-        link:camb,
-       src:camb
-    },{
-        jobtitle:"Spoken Toturial",
-        where:"IIT Bombay",
-        date:2021,
-        link:spktl,
-        src:spktl
-    }
+    jobtitle: "Technology Job Simulation",
+    where: "Deloitte",
+    date: "2024",
+    link: "https://drive.google.com/file/d/1fL20tMjdZN-x6DThw1ynxlCxRzWdUhqN/view?usp=sharing",
+    src: deloitte
+}, {
+    jobtitle: "Advanced Software Engineering Virtual Experience Program",
+    where: "(Walmart Global Tech)",
+    date: "2023",
+    link: "https://drive.google.com/file/d/1ULl-qAOyFpatIcZXsWdRNt_u-clJGxui/view?usp=sharing",
+    src: walmart
+},
+{
+    jobtitle: "Accenture Nordics Virtual Experience Program",
+    where: "Forage(Developer)",
+    date: "2023",
+    link: "https://drive.google.com/file/d/1dYLcCCmC6LnG1YlCU7qCJaI24zPbLaf3/view?usp=sharing",
+    src: accent
+},
+{
+    jobtitle: "Certified Cloud Computing ",
+    where: "(Europe Academy)",
+    date: "2021",
+    link: "https://drive.google.com/file/d/1zs10WXvnmVA8_awXAvDXq43tYaCNZfqX/view?usp=sharing",
+    src: cloud
+},
+{
+    jobtitle: "Business English",
+    where: "Cambridge English",
+    date: "2023",
+    link: camb,
+    src: camb
+},
+{
+    jobtitle: "Effective Writting",
+    where: "NPTEL Online Certification",
+    date: "Jan-Feb 2024",
+    link: nptel,
+    src: nptel
+}
 ];
 
 const skills = [{
-        name: "React"
-    },
-    {
-        name: "HTML & CSS"
-    },
-    {
-        name: "Javascript"
-    },
-    {
-        name: "JAVA"
-    },
-    {
-        name: "SQL"
-    },
-    {
-        name: "MS office"
-    },
-    {
-        name:"Ubuntu"
-    }
+    name: "React"
+},
+{
+    name: "HTML & CSS"
+},
+{
+    name: "Javascript"
+},
+{
+    name: "JAVA"
+},
+{
+    name: "SQL"
+},
+{
+    name: "MS office"
+},
+{
+    name: "Ubuntu"
+}
 ];
 
 const services = [{
-        title: "Full Stack Developement Intern",
-        description: "Bharat Intern",
-    }
+    title: "Full Stack Developement Intern",
+    description: "Bharat Intern",
+}
 ];
 
 const dataportfolio = [{
-        img: thumbnail,
-        description: "Food Ordering Application, Due to own database fetch couldn't host it online",
-        link: "https://drive.google.com/file/d/1H-MCJsrE_MEkfIpoT4FXSSOXbfQxUAXc/view?usp=sharing",
-    },{
-        img: tothumb,
-        description:"To Do List with browser storage",
-        link:"https://first-react-o0uuaa8p8-rozrc25.vercel.app/"
-    }
+    img: thumbnail,
+    description: "Food Ordering Application",
+    link: "https://drive.google.com/file/d/1H-MCJsrE_MEkfIpoT4FXSSOXbfQxUAXc/view?usp=sharing",
+}
 ];
 
 const contactConfig = {
-    YOUR_EMAIL:"rohithfaizal25@yahoo.com",
+    YOUR_EMAIL: "rohithfaizal25@yahoo.com",
     description: " ''Feel free to reach out to me via email or connect with me on LinkedIn. I'm always open to engaging in meaningful conversations and expanding my professional network. Please don't hesitate to contact me if you have any questions or would like to discuss potential opportunities. I look forward to connecting with you!'' ",
     YOUR_SERVICE_ID: "service_4qs2zaa",
     YOUR_TEMPLATE_ID: "template_krmsggl",
