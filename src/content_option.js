3dd38e68a9fbe3ff577e0c9f476b9fef5a9b39ce
Rohith <98480349+Rozrc25@users.noreply.github.com>
2024-09-04 12:32:49 +0530

    Add files via upload

--- src/content_option.js
+++ src/content_option.js
@@ -6,6 +6,7 @@ import cloud from "./assets/images/europ cloud.jpg"
 import camb from "./assets/images/BEC interm.jpg"
 import nptel from "./assets/images/nptel.jpg"
 import deloitte from "./assets/images/Deloitte=Forage.png"
+import JC from "./assets/images/JourneyCraft.jpg"
 var ceticon = "https://img.favpng.com/6/20/9/diploma-higher-education-logo-academic-certificate-png-favpng-t9LrfKwr2GdHpV088Ph3dQ5NF.jpg"
 
 const logotext = "ROZRC25";
@@ -28,7 +29,7 @@ const introdata = {
 
 const dataabout = {
     title: "about my self",
-    aboutme: "I am Rohith, a final-year B.Tech CSE student. Driven by a passion for technology, I possess a strong foundation in programming, software development, and data structures. With excellent problem-solving abilities and effective communication skills, I thrive in dynamic environments. Eager to contribute to innovative projects, I am dedicated to continuous learning and staying ahead in the rapidly evolving tech industry.",
+    aboutme: "I am Rohith, B.Tech CSE Graduate. Driven by a passion for technology, I possess a strong foundation in programming, software development, and data structures. With excellent problem-solving abilities and effective communication skills, I thrive in dynamic environments. Eager to contribute to innovative projects, I am dedicated to continuous learning and staying ahead in the rapidly evolving tech industry.",
 };
 const certificates = [{
     jobtitle: "Technology Job Simulation",
@@ -77,6 +78,9 @@ const skills = [{
     name: "React"
 },
 {
+    name: "React Native"
+},
+{
     name: "HTML & CSS"
 },
 {
@@ -93,7 +97,8 @@ const skills = [{
 },
 {
     name: "Ubuntu"
-}
+},
+
 ];
 
 const services = [{
@@ -105,7 +110,12 @@ const services = [{
 const dataportfolio = [{
     img: thumbnail,
     description: "Food Ordering Application",
-    link: "https://drive.google.com/file/d/1H-MCJsrE_MEkfIpoT4FXSSOXbfQxUAXc/view?usp=sharing",
+    link: "https://github.com/Rozrc25/rozFoodProject",
+},
+{
+    img: JC,
+    description: "AI Travel Plan Generator App",
+    link: "https://github.com/Rozrc25/JourneyCraft",
 }
 ];
 
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -4,6 +4,10 @@ import { Helmet, HelmetProvider } from "react-helmet-async";
 import Typewriter from "typewriter-effect";
 import { introdata, meta } from "../../content_option";
 import { Link } from "react-router-dom";
+import Marquee from "react-fast-marquee";
+import { FiDownload } from "react-icons/fi";
+
+
 
 export const Home = () => {
   return (
@@ -42,7 +46,7 @@ export const Home = () => {
                 <div className="intro_btn-action pb-5">
                   <Link to="/portfolio" className="text_2">
                     <div id="button_p" className="ac_btn btn ">
-                    Practice Work
+                      Practice Work
                       <div className="ring one"></div>
                       <div className="ring two"></div>
                       <div className="ring three"></div>
@@ -57,6 +61,30 @@ export const Home = () => {
                     </div>
                   </Link>
                 </div>
+
+                <h2 className="mb-1x">Tech Stack</h2>
+
+
+                <Marquee direction="right" speed={100} style={{ display: "flex", flexDirection: 'row', justifyContent: 'space-between', gap: '20', alignItems: 'center' }} gradient='15' gradientWidth={30} gradientColor="var(--primary-color)">
+                  <img className="stackimg" src="https://cdn-icons-png.flaticon.com/128/5968/5968267.png" style={{ height: 90, width: 95 }} alt="HTML"></img>
+                  <img className="stackimg" src="https://cdn-icons-png.flaticon.com/128/732/732190.png" alt="CSS" ></img>
+                  <img className="stackimg" src="https://cdn-icons-png.flaticon.com/128/5968/5968292.png" alt="JS" ></img>
+                  <img className="stackimg" src="https://cdn-icons-png.flaticon.com/128/1260/1260667.png" alt="React" ></img>
+                  <img className="stackimg" src="https://cdn-icons-png.flaticon.com/128/226/226777.png" alt="java" ></img>
+                  <img className="stackimg" src="https://cdn-icons-png.flaticon.com/128/15466/15466163.png" alt="git" ></img>
+                  <img className="stackimg" src="https://cdn-icons-png.flaticon.com/128/733/733553.png" alt="github" ></img>
+                  <img className="stackimg" src="https://cdn-icons-png.flaticon.com/128/5968/5968313.png" alt="mysql" ></img>
+                  <img className="stackimg" src="https://hygraph.com/icons/icon-512x512.png" alt="hygraph" ></img>
+                  <img className="stackimg" src="https://cdn4.iconfinder.com/data/icons/google-i-o-2016/512/google_firebase-2-512.png" alt="firebase" ></img>
+                </Marquee>
+                <div className="button_h" style={{marginTop:20 }}>
+                  <a className="ac_btn btn" target="blank" href="https://drive.google.com/file/d/11uUAJeL6W6emQihwZmG7k0yKEzebKqpL/view?usp=sharing" style={{ textDecoration: 'none' ,display:'flex' ,gap:5,width:'fit-content'}} download ><FiDownload style={{ fontSize: 20 }} />Downlode Resume</a>
+                  <div className="ring one"></div>
+                  <div className="ring two"></div>
+                  <div className="ring three"></div>
+                </div>
+
+
               </div>
             </div>
           </div>
